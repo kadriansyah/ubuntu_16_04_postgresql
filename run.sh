2ed1b4cd6389d0df258f068a383b51aaa17dd61d
Kiagus Arief Adriansyah <arief@alodokter.com>
2019-11-08 07:00:55 +0700

    using official image

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Docker Managed Volume
-docker run -d --name postgresql -v postgresql_data1:/etc/postgresql \
-                                -v postgresql_data2:/var/log/postgresql \
-                                -v postgresql_data3:/var/lib/postgresql \
-                                -p 5432:5432 kadriansyah/postgresql
+docker run -d --name postgresql -v postgresql_data:/var/lib/postgresql/data -p 5432:5432 kadriansyah/postgresql
 
 # # Bind Mount Volume
-# docker run -d --name mongo -v /Users/kadriansyah/projects/data:/data -p 5432:5432 kadriansyah/mongo
+# docker run -d --name mongo -v /Users/kadriansyah/projects/data:/var/lib/postgresql/data -p 5432:5432 kadriansyah/mongo
